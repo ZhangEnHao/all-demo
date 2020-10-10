dd25ef8a4a23f945ac0950ac7e5bb5e2f7341ef7
enhao <13453727099@163.com>
2020-10-10 18:26:33 +0800

    20201010

--- src/pages/PageControl/Controls/ParameterEntry/RuleItem.js
+++ src/pages/PageControl/Controls/ParameterEntry/RuleItem.js
@@ -1,10 +1,143 @@
 import React, { Component } from 'react';
-import { Row, Col, Form, Radio, Input, Switch, InputNumber, } from 'antd';
+import { Row, Col, Form, Radio, Input, Switch, InputNumber, Checkbox, Button, } from 'antd';
 
 const { Item } = Form;
 const formItemLayout = {
   labelCol: { span: 5 },
   wrapperCol: { span: 19 },
+}
+
+// 
+class StartStop extends Component {
+  state = {
+    disableByEnglish: true,
+    disableByOther: true,
+  }
+
+  onCheckboxChange = (e, type) => {
+    this.triggerChange({ [type]: e.target.checked });
+  }
+
+  onRadioGroupChange = e => {
+    if(e.target.value === "english") {
+      this.setState({disableByEnglish: false})
+    }else if(e.target.value === "other") {
+      this.setState({disableByOther: false})
+    }else {
+      this.setState({disableByEnglish: true, disableByOther: true, })
+    }
+    this.triggerChange({ checktype: e.target.value });
+  }
+
+  otherValueChange = e => {
+    this.triggerChange({ otherValue: e.target.value });
+  }
+
+  triggerChange = changedValue => {
+    const { onChange, value } = this.props;
+    if (onChange) {
+      onChange({
+        ...value,
+        ...changedValue,
+      });
+    }
+  };
+
+  render() {
+    // let { value } = this.props;
+    return (
+      <div style={{ display: "flex", height: 40, alignItems: "center" }}>
+        <Radio.Group onChange={this.onRadioGroupChange} style={{ flex: "0 0 auto" }}>
+          <Radio value="english">英文</Radio>
+          <Checkbox onChange={e => this.onCheckboxChange(e, "uppercase")} disabled={this.state.disableByEnglish}>大写</Checkbox>
+          <Checkbox onChange={e => this.onCheckboxChange(e, "lowercase")} disabled={this.state.disableByEnglish}>小写</Checkbox>
+          <Radio value="number">数字</Radio>
+          <Radio value="other">其他</Radio>
+        </Radio.Group>
+        <Input
+          // value={value.otherValue}
+          onChange={this.otherValueChange}
+          disabled={this.state.disableByOther}
+          style={{ flex: "1 1 auto", marginRight: 10, }} />
+        <Button type="danger" style={{ flex: "0 0 auto" }}>清除</Button>
+      </div>
+    )
+  }
+}
+
+//  或/且 规则组合
+class CheckboxList extends Component {
+  state = {
+    checkboxs: [
+      { label: "英文", value: "english", child: [1, 2] },
+      { label: "大写", value: "uppercase", disabled: true },
+      { label: "小写", value: "lowercase", disabled: true },
+      { label: "数字", value: "number", child: [4] },
+      { label: "非零", value: "nonzero", disabled: true },
+      { label: "其他", value: "other", },
+    ],
+    disableByOther: true,
+  }
+
+  checkGroupChange = (checkedValues) => {
+    /*****   控制是否可点击选中   *****/
+    // 英文/数字 控制其后
+    let controlList = checkedValues.filter(item => ["english", "number"].indexOf(item) > -1);
+    this.state.checkboxs.forEach((checkbox, index) => {
+      this.setState(state => {
+        let checkboxs = state.checkboxs;
+        checkbox.child && checkbox.child.forEach(item => {
+          if (controlList.indexOf(checkbox.value) > -1) {
+            checkboxs[item].disabled = false;
+          } else {
+            checkboxs[item].disabled = true;
+          }
+        })
+        return { checkboxs }
+      })
+    })
+    // 其他 控制 其他输入框
+    if (checkedValues.indexOf("other") > -1) {
+      this.setState({ disableByOther: false })
+    } else {
+      this.setState({ disableByOther: true })
+    }
+
+    this.triggerChange({ checkValue: checkedValues });
+  }
+
+  otherValueChange = e => {
+    this.triggerChange({ otherValue: e.target.value });
+  }
+
+  triggerChange = changedValue => {
+    const { onChange, value } = this.props;
+    if (onChange) {
+      onChange({
+        ...value,
+        ...changedValue,
+      });
+    }
+  };
+
+  render() {
+    const { value } = this.props;
+    return (
+      <div style={{ display: "flex", height: 40, alignItems: "center" }}>
+        <Checkbox.Group
+          style={{ flex: "0 0 auto" }}
+          options={this.state.checkboxs}
+          value={value.checkValue}
+          onChange={this.checkGroupChange}
+        />
+        <Input
+          value={value.otherValue}
+          onChange={this.otherValueChange}
+          disabled={this.state.disableByOther}
+          style={{ flex: "1 1 auto" }} />
+      </div>
+    )
+  }
 }
 
 // 长度约束
@@ -29,7 +162,7 @@ class LengthConstraint extends Component {
       <>
         <InputNumber value={value.min} min={0} onChange={this.handleMinChange} style={{ width: '30%', marginRight: '5%' }} />
         至
-        <InputNumber value={value.max} min={value.min+1} onChange={this.handleMaxChange} style={{ width: '30%', marginRight: '5%', marginLeft: '5%' }} />
+        <InputNumber value={value.max} min={value.min + 1} onChange={this.handleMaxChange} style={{ width: '30%', marginRight: '5%', marginLeft: '5%' }} />
       </>
     );
   }
@@ -69,23 +202,23 @@ class ItemByString extends Component {
       </Row>
       <Item label={<span>全部为<span style={{ color: "red" }}>[或]</span></span>} {...formItemLayout}>
         {getFieldDecorator('mustIn', {
-          // initialValue: ,
-        })(<Input />)}
+          initialValue: { checkValue: [], otherValue: null },
+        })(<CheckboxList />)}
       </Item>
       <Item label={<span>必须含<span style={{ color: "red" }}>[且]</span></span>} {...formItemLayout}>
         {getFieldDecorator('mustContain', {
-          // initialValue: ,
-        })(<Input />)}
+          initialValue: { checkValue: [], otherValue: null },
+        })(<CheckboxList />)}
       </Item>
       <Item label="字符开头" {...formItemLayout}>
         {getFieldDecorator('startWith', {
           // initialValue: ,
-        })(<Input />)}
+        })(<StartStop />)}
       </Item>
       <Item label="字符结尾" {...formItemLayout}>
         {getFieldDecorator('endWith', {
           // initialValue: ,
-        })(<Input />)}
+        })(<StartStop />)}
       </Item>
       <Item label="不包含字符" {...formItemLayout}>
         {getFieldDecorator('notContain', {
@@ -94,7 +227,7 @@ class ItemByString extends Component {
       </Item>
       <Item label="长度约束" {...formItemLayout}>
         {getFieldDecorator('stringLength', {
-          initialValue: { min: null, max: null } ,
+          initialValue: { min: null, max: null },
         })(<LengthConstraint />)}
       </Item>
     </>
